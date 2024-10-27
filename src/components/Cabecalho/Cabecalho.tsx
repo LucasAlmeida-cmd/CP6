0244859b49ce27d1ded21e5839fa1a7171ddf3c0
victorcosmedev <victorcosme.profissional@gmail.com>
2024-10-27 00:45:20 -0300

    Formatação do cabeçalho feita com sucesso
    Co-authored-by: Lucas-CMD <lucasalmeidafmoraes@gmail.com>
    Co-authored-by: arthur-pulini
    <arthurpulini@gmail.com>

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,10 @@
 import Menu from "../Menu/Menu";
 
 export default function Cabecalho() {
-    return (
-        <header className="cabecalho">
-            <Menu />
-        </header>
-    )
+  return (
+    <header className="flex lg:px-[7vw] lg:py-5 lg:font-bold lg:gap-[5vw] lg:justify-start hover:shadow-md ease-in duration-300 items-center sm:px-[7vw] sm:py-5 sm:font-bold sm:gap-[5vw] sm:justify-between border">
+      <h1 className="lg:text-xl text-blue-800">QuantumLeap</h1>
+      <Menu />
+    </header>
+  );
 }
