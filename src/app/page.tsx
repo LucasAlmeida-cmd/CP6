7e1debcd76d10fd2db0337f0e4b4371e8ac579e7
victorcosmedev <victorcosme.profissional@gmail.com>
2024-10-27 00:39:18 -0300

    Adicionados os cards de redirecionamento para as páginas de GS, CPs e sprints na home.
    Co-authored-by: Lucas-CMD <lucasalmeidafmoraes@gmail.com>
    Co-authored-by: arthur-pulini
    <arthurpulini@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,67 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
+    <main className="px-[10%] py-[15vh] flex flex-col gap-[5vh] justify-between">
+      <div className="flex lg:flex-row sm:flex-col justify-between w-full gap-3">
+        <Link href="/notas/notas-cps/cps">
+          <div className="border hover:border-blue-800 rounded flex flex-col transition ease-in-out lg:px-4 lg:py-2 hover:shadow-md">
+            <div>
+              <h2 className="font-semibold w-full text-center">
+                Checkpoints
+              </h2>
+              <p className="break-words text-center">
+                Acesse aqui as nossas notas de Checkpoints. 
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link href="/notas/notas-gs/gs">
+          <div className="border hover:border-blue-800 rounded lg:px-4 lg:py-2 flex flex-col hover:shadow-md transition ease-in-out ">
+            <div>
+              <h2 className="font-semibold w-full text-center">
+                Global Solution
+              </h2>
+              <p className="break-words text-center">
+                Acesse aqui as nossas notas na Global Solution.
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link href="/notas/notas-gs/gs">
+          <div className="border hover:border-blue-800 rounded lg:px-4 lg:py-2 flex flex-col hover:shadow-md transition ease-in-out ">
+            <h2 className="font-semibold w-full text-center">
+              Challenge Sprints
+            </h2>
+            <p className="break-words text-center">
+              Acesse aqui as nossas notas nas sprints do Challenge.
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className="w-full border transition ease-in-out hover:border-blue-800 rounded py-5 px-[3vw]">
+        <h1 className="font-semibold mb-5 lg:text-xl">Médias do Grupo por Tipo de Avaliação</h1>
+        <div className="flex flex-col gap-3">
+          {/* div das barras */}
 
-      <h1>Falar algumas coisas aqui</h1>
-    </div>
+          <div className="flex gap-3 items-center">
+            <h1 className="font-medium block w-[10%] break-words">Checkpoints</h1>
+            <div className="bg-blue-800 h-5 w-[60%] rounded-r-md"></div>
+            <p>Nota</p>
+          </div>
+          <div className="flex gap-3 items-center">
+            <h1 className="font-medium block w-[10%] break-words">Global Solution</h1>
+            <div className="bg-blue-800 h-5 w-[75%] rounded-r-md"></div>
+            <p>Nota</p>
+          </div>
+          <div className="flex gap-3 items-center">
+            <h1 className="font-medium block w-[10%] break-words">Challenge Sprints</h1>
+            <div className="bg-blue-800 h-5 w-[80%] rounded-r-md"></div>
+            <p>Nota</p>
+          </div>
+          
+        </div>
+      </div>
+    </main>
   )
 }
